442d4a77faed8217976a9aa7737fffada0602b3b
brandon-yipitdata <bemmerich@yipitdata.com>
2018-08-23 18:22:08 -0400

    Implement more elegant looping mechanism to hit keywords

--- bin.py
+++ bin.py
@@ -1,23 +1,23 @@
+#! /anaconda2/bin/python
+# -*- coding: utf-8 -*-
+
 import pandas as pd
 import send_me_email
 import twitter_data
 
-insights = twitter_data.get_twitter_data(keyword = 'Alternative Data insights', count = 50).to_html(index = False)
-capital = twitter_data.get_twitter_data(keyword = 'Alternative data capital', count = 50).to_html(index = False)
-financial = twitter_data.get_twitter_data(keyword = 'Alternative data financial', count = 50).to_html(index = False)
-research = twitter_data.get_twitter_data(keyword = 'Alternative data research', count = 50).to_html(index = False)
-china = twitter_data.get_twitter_data(keyword = 'Alternative data china', count = 50).to_html(index = False)
-careers = twitter_data.get_twitter_data(keyword = 'Alternative data careers', count = 50).to_html(index = False)
-company = twitter_data.get_twitter_data(keyword = 'Alternative data company', count = 50).to_html(index = False)
-services = twitter_data.get_twitter_data(keyword = 'Alternative data services', count = 50).to_html(index = False)
-datasets = twitter_data.get_twitter_data(keyword = 'Alternative data datasets', count = 50).to_html(index = False)
+# Manually input keywords to keep abreast of
+keyword_list = [('Insights', 'Alternative Data Insights'), ('Capital','Alternative Data capital'),('Financial','Alternative Data Financial'),('Research','Alternative Data Research'),('China','Alternative Data China'),('Careers','Alternative Data Careers'),('Company','Alternative Data Company'),('Services','Alternative Data Services'),('Dataset','Alternative Dataset')]
+message = u''
 
+# For each keyword, hit the twitter api to get matched tweet text and links to each tweet
+for description, keyword in keyword_list:
+    results = twitter_data.get_twitter_data(keyword = keyword, count = 50).to_html(index = False)
+    message += u'{}:{}\n\n'.format(description, results)
 
 subject = "Alt Data Sourcing Bot"
 recipients = ["bemmerich@yipitdata.com", 'jkelly@yipitdata.com','mrondinaro@yipitdata.com']
-message = u'Insights:{}\n\nCapital:{}\n\nFinancial:{}\n\nresearch:{}\n\nChina:{}\n\nCareers:{}\n\nCompany:{}\n\nServices:{}\n\nDatasets:{}'.format(insights, capital, financial, research, china, careers, company, services, datasets)
 
-
+# Send emails to all particpants in the project
 for recipient in recipients:
     print recipient
     send_me_email.send_html_email(message, recipient, subject)
