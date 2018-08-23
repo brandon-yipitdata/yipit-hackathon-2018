5bf2417f3dcc0e5c6d71308f87594afb33f7b8be
brandon-yipitdata <bemmerich@yipitdata.com>
2018-08-23 17:28:18 -0400

    Add new search terms

--- bin.py
+++ bin.py
@@ -2,13 +2,20 @@ import pandas as pd
 import send_me_email
 import twitter_data
 
-alt_data = twitter_data.get_twitter_data(keyword = 'Alternative Data', count = 50).to_html(index = False)
-sat_data = twitter_data.get_twitter_data(keyword = 'Satellite Data', count = 50).to_html(index = False)
+insights = twitter_data.get_twitter_data(keyword = 'Alternative Data insights', count = 50).to_html(index = False)
+capital = twitter_data.get_twitter_data(keyword = 'Alternative data capital', count = 50).to_html(index = False)
+financial = twitter_data.get_twitter_data(keyword = 'Alternative data financial', count = 50).to_html(index = False)
+research = twitter_data.get_twitter_data(keyword = 'Alternative data research', count = 50).to_html(index = False)
+china = twitter_data.get_twitter_data(keyword = 'Alternative data china', count = 50).to_html(index = False)
+careers = twitter_data.get_twitter_data(keyword = 'Alternative data careers', count = 50).to_html(index = False)
+company = twitter_data.get_twitter_data(keyword = 'Alternative data company', count = 50).to_html(index = False)
+services = twitter_data.get_twitter_data(keyword = 'Alternative data services', count = 50).to_html(index = False)
+datasets = twitter_data.get_twitter_data(keyword = 'Alternative data datasets', count = 50).to_html(index = False)
 
 
 subject = "Alt Data Sourcing Bot"
 recipients = ["bemmerich@yipitdata.com", 'jkelly@yipitdata.com','mrondinaro@yipitdata.com']
-message = u'Alternative Data:{}\n\nSatellite Data:{}'.format(alt_data, sat_data)
+message = u'Insights:{}\n\nCapital:{}\n\nFinancial:{}\n\nresearch:{}\n\nChina:{}\n\nCareers:{}\n\nCompany:{}\n\nServices:{}\n\nDatasets:{}'.format(insights, capital, financial, research, china, careers, company, services, datasets)
 
 
 for recipient in recipients:
